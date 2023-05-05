9e0b6899656ce26803d0e69787e1163f3c789592
遇到困难呼噜噜 <wei2836377438@163.com>
2023-05-05 13:18:42 +0800

    Site updated: 2023-05-05 13:18:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/04/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/04/hello-world/","2023/05/04/link/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
